e12a906961250d4218623d17e67a9400f7614250
ProfessionalGriefer <vincentnahn@gmail.com>
2024-10-30 21:55:10 +0000

    quickposition 0.1.1

--- Formula/quickposition.rb
+++ Formula/quickposition.rb
@@ -1,8 +1,8 @@
 class Quickposition < Formula
   desc "Quickly position an iPad to the left or right of a Mac display"
   homepage ""
-  url "https://github.com/ProfessionalGriefer/quickposition/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "09197f61db61f54309e3e78b1c22158181c2ba6d485cb080c6248199939b021d"
+  url "https://github.com/ProfessionalGriefer/quickposition/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "a4ec32ef162e4b338772b580b2fb467450c4f1edb19c32b5a3448d518e80af82"
   license "MIT"
 
   depends_on "displayplacer"
